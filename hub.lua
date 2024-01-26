8cef3dd0f4331d1673f06011830bbec7679f896f
ManHackWiz <119904470+ManHackWiz@users.noreply.github.com>
2024-01-26 15:01:18 -0500

    Create hub.lua

---
+++ hub.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Anti-AliHub", "BloodTheme")
+
+local Tab_Exp = Window:NewTab("Exploits")
+local Extras = Window:NewTab("Scripts")
+
+local Exploits = Tab:NewSection("Exploits")
+
+Exploits:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
+
+local Extra_Section = Tab:NewSection("Extras")
+
+Extra_Section:NewButton("Infinite Yield", "Executes Infinite Yield", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+Extra_Section:NewButton("Dex", "Executes DEX to view game explorer", function()
+    
+end)
